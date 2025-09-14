d868d50a716cb95684755bfe1a4c5c95e64ff460
juli-anne <julianne.lazar@gmail.com>
2025-09-14 12:26:43 +0200

    add authenticated endpoits for admin

---
+++ src/main/java/com/julianne/pets/controllers/SecurityController.java
@@ -0,0 +1,18 @@
+package com.julianne.pets.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/open")
+    public String open() {
+        return "open";
+    }
+
+    @GetMapping("/closed")
+    public String closed() {
+        return "closed";
+    }
+}
